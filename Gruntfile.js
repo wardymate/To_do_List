737ae3be46036a8c8ec88a3c29a1d41cb0c24b2d
wardymate <chriswarddev@gmail.com>
2015-03-21 10:34:25 +0000

    grunt runs selenium drive automatically

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,27 +6,38 @@ module.exports = function(grunt){
       all: ['Gruntfile.js', 'src/**/*.js', 'test/*.js']
     },
     protractor: {
-    options: {
-      configFile: "conf.js", // Default config file
-      keepAlive: true, // If false, the grunt process stops when the test fails.
-      noColor: false, // If true, protractor will not use colors in its output.
-      args: {
-        // Arguments passed to the command
-      }
-    },
-    your_target: {   // Grunt requires at least one target to run so you can simply put 'all: {}' here too.
       options: {
-        configFile: "conf.js", // Target-specific config file
-        args: {} // Target-specific arguments
-      }
+        configFile: "conf.js", // Default config file
+        keepAlive: true, // If false, the grunt process stops when the test fails.
+        noColor: false, // If true, protractor will not use colors in its output.
+        args: {
+          // Arguments passed to the command
+        }
+      },
+      your_target: {   // Grunt requires at least one target to run so you can simply put 'all: {}' here too.
+        options: {
+          configFile: "conf.js", // Target-specific config file
+          args: {} // Target-specific arguments
+        }
+      },
+    },
+    protractor_webdriver: {
+    options: {
+      // Task-specific options go here.
+    },
+    your_target: {
+      command: 'webdriver-manager start',// Target-specific file lists and/or options go here.
     },
   },
+
 
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-protractor-webdriver');
   grunt.loadNpmTasks('grunt-protractor-runner');
 
-  grunt.registerTask('default', ['jshint','protractor']);
+
+  grunt.registerTask('default', ['jshint','protractor_webdriver','protractor']);
 
 };
