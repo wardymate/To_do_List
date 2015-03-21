6d6706e3ea65c43a0e3003ad5f13f08ba76e5261
wardymate <chriswarddev@gmail.com>
2015-03-21 08:13:54 +0000

    added grunt to automate protractor and linter

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt){
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      all: ['Gruntfile.js', 'src/**/*.js', 'test/*.js']
+    },
+    protractor: {
+    options: {
+      configFile: "conf.js", // Default config file
+      keepAlive: true, // If false, the grunt process stops when the test fails.
+      noColor: false, // If true, protractor will not use colors in its output.
+      args: {
+        // Arguments passed to the command
+      }
+    },
+    your_target: {   // Grunt requires at least one target to run so you can simply put 'all: {}' here too.
+      options: {
+        configFile: "conf.js", // Target-specific config file
+        args: {} // Target-specific arguments
+      }
+    },
+  },
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-protractor-runner');
+
+  grunt.registerTask('default', ['jshint','protractor']);
+
+};
--- conf.js
+++ conf.js
@@ -1,4 +1,9 @@
 exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['test/spec.js']
+  specs: ['test/spec.js'],
+  // baseUrl: 'http://localhost:8000',
+  maxSessions: 1,
+  multiCapabilities: [
+    { browserName: 'chrome' }
+  ],
 }
