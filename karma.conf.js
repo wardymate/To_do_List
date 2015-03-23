cd470ecad700c2e1c8b4f755112d8e291cc0d6da
wardymate <chriswarddev@gmail.com>
2015-03-23 09:11:49 +0000

    second unti test passes first test now fails

--- karma.conf.js
+++ karma.conf.js
@@ -19,6 +19,7 @@ module.exports = function(config) {
       'public/bower_components/angular-route/angular-route.js',
       'public/bower_components/angular-resource/angular-resource.js',
       'public/bower_components/angular-mocks/angular-mocks.js',
+      'public/js/app.js',
       'public/js/*.js',
       'test/unit/*.js'
     ],
